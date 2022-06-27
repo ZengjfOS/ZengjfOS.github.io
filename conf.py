f49b402305b51d877faf4845a9fb413b5531cacc
zengjf <zengjf42@163.com>
2022-06-28 06:59:51 +0800

    update title

--- conf.py
+++ conf.py
@@ -55,6 +55,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_title = 'zengjf'
+html_short_title = 'zengjf'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
